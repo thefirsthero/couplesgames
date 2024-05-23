9b56f4832a732c2b4f7438f99fabbdc93b0eb451
thefirsthero <vukosi90@gmail.com>
2024-05-24 00:28:11 +0200

    removing srecretstr

--- api/models/user.py
+++ api/models/user.py
@@ -1,10 +1,9 @@
-from pydantic import BaseModel, EmailStr, SecretStr, Field
-from typing import Optional
+from pydantic import BaseModel, EmailStr, Field
 
 class User(BaseModel):
     username: str = Field(..., min_length=3, max_length=20)
     email: EmailStr | None = Field(default=None)
-    password: SecretStr = Field(..., min_length=3, max_length=20)
+    password: str = Field(..., min_length=3, max_length=20)
 
 class UserInDB(User):
     hashed_password: str
