8b5dfeb4e78e5587d8e503a0bd7e91baf1f1c9b0
thefirsthero <vukosi90@gmail.com>
2024-05-23 22:24:00 +0200

    renaming firebase_config.py to firebase.py

--- api/data/firebase.py
+++ api/data/firebase.py
@@ -1,14 +1,17 @@
-# firebase.py
-
+import base64
 import os
 from firebase_admin import credentials, firestore, initialize_app
+
+# Decode the Base64 encoded private key
+encoded_key = os.getenv("FIREBASE_PRIVATE_KEY_BASE64")
+decoded_key = base64.b64decode(encoded_key).decode('utf-8')
 
 # Read Firebase credentials from environment variables
 firebase_credentials = {
     "type": "service_account",
     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
     "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "private_key": decoded_key,
     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -17,6 +20,7 @@ firebase_credentials = {
     "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
 }
 
+print('private_key:', firebase_credentials.get('private_key'))
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate(firebase_credentials)
 initialize_app(cred)
