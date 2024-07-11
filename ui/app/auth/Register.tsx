a5bc3f1d59b321c65b00e58f13c7b4093b70435b
thefirsthero <vukosi90@gmail.com>
2024-07-11 18:41:16 +0200

    base auth screen and components setup

---
+++ ui/app/auth/Register.tsx
@@ -0,0 +1,111 @@
+import React, { memo, useState } from 'react'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text } from 'react-native-paper'
+import Background from '@/components/auth/Background'
+import Logo from '@/components/auth/Logo'
+import Header from '@/components/auth/Header'
+import Button from '@/components/auth/Button'
+import TextInput from '@/components/auth/TextInput'
+import BackButton from '@/components/auth/BackButton'
+// import { theme } from '../core/theme'
+import { emailValidator } from '@/utils/emailValidator'
+import { passwordValidator } from '@/utils/passwordValidator'
+import { nameValidator } from '@/utils/nameValidator'
+import { Navigation } from '@/types/Navigation'
+
+type Props = {
+  navigation: Navigation;
+};
+
+const RegisterScreen = ({ navigation }: Props) => {
+  const [name, setName] = useState({ value: '', error: '' });
+  const [email, setEmail] = useState({ value: '', error: '' });
+  const [password, setPassword] = useState({ value: '', error: '' });
+
+  const _onSignUpPressed = () => {
+    const nameError = nameValidator(name.value);
+    const emailError = emailValidator(email.value);
+    const passwordError = passwordValidator(password.value);
+
+    if (emailError || passwordError || nameError) {
+      setName({ ...name, error: nameError });
+      setEmail({ ...email, error: emailError });
+      setPassword({ ...password, error: passwordError });
+      return;
+    }
+
+    navigation.navigate('Dashboard');
+  };
+
+  return (
+    <Background>
+      <BackButton goBack={() => navigation.navigate('HomeScreen')} />
+
+      <Logo />
+
+      <Header>Create Account</Header>
+
+      <TextInput
+        label="Name"
+        returnKeyType="next"
+        value={name.value}
+        onChangeText={text => setName({ value: text, error: '' })}
+        error={!!name.error}
+        errorText={name.error}
+      />
+
+      <TextInput
+        label="Email"
+        returnKeyType="next"
+        value={email.value}
+        onChangeText={text => setEmail({ value: text, error: '' })}
+        error={!!email.error}
+        errorText={email.error}
+        autoCapitalize="none"
+        autoComplete="email"
+        textContentType="emailAddress"
+        keyboardType="email-address"
+      />
+
+      <TextInput
+        label="Password"
+        returnKeyType="done"
+        value={password.value}
+        onChangeText={text => setPassword({ value: text, error: '' })}
+        error={!!password.error}
+        errorText={password.error}
+        secureTextEntry
+      />
+
+      <Button mode="contained" onPress={_onSignUpPressed} style={styles.button}>
+        Sign Up
+      </Button>
+
+      <View style={styles.row}>
+        <Text style={styles.label}>Already have an account? </Text>
+        <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')}>
+          <Text style={styles.link}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </Background>
+  );
+};
+
+const styles = StyleSheet.create({
+  label: {
+    // color: theme.colors.secondary,
+  },
+  button: {
+    marginTop: 24,
+  },
+  row: {
+    flexDirection: 'row',
+    marginTop: 4,
+  },
+  link: {
+    fontWeight: 'bold',
+    // color: theme.colors.primary,
+  },
+});
+
+export default memo(RegisterScreen);
---
+++ ui/app/auth/ResetPassword.tsx
@@ -0,0 +1,80 @@
+import React, { memo, useState } from 'react';
+import { Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { emailValidator } from '@/utils/emailValidator'
+import TextInput from '@/components/auth/TextInput'
+import BackButton from '@/components/auth/BackButton'
+import Background from '@/components/auth/Background'
+import Logo from '@/components/auth/Logo'
+import Header from '@/components/auth/Header'
+// import { theme } from '../core/theme';
+import Button from '@/components/auth/Button'
+import { Navigation } from '@/types/Navigation'
+
+type Props = {
+  navigation: Navigation;
+};
+
+const ForgotPasswordScreen = ({ navigation }: Props) => {
+  const [email, setEmail] = useState({ value: '', error: '' });
+
+  const _onSendPressed = () => {
+    const emailError = emailValidator(email.value);
+
+    if (emailError) {
+      setEmail({ ...email, error: emailError });
+      return;
+    }
+
+    navigation.navigate('LoginScreen');
+  };
+
+  return (
+    <Background>
+      <BackButton goBack={() => navigation.navigate('LoginScreen')} />
+
+      <Logo />
+
+      <Header>Restore Password</Header>
+
+      <TextInput
+        label="E-mail address"
+        returnKeyType="done"
+        value={email.value}
+        onChangeText={text => setEmail({ value: text, error: '' })}
+        error={!!email.error}
+        errorText={email.error}
+        autoCapitalize="none"
+        autoComplete="email"
+        textContentType="emailAddress"
+        keyboardType="email-address"
+      />
+
+      <Button mode="contained" onPress={_onSendPressed} style={styles.button}>
+        Send Reset Instructions
+      </Button>
+
+      <TouchableOpacity
+        style={styles.back}
+        onPress={() => navigation.navigate('LoginScreen')}
+      >
+        <Text style={styles.label}>← Back to login</Text>
+      </TouchableOpacity>
+    </Background>
+  );
+};
+
+const styles = StyleSheet.create({
+  back: {
+    width: '100%',
+    marginTop: 12,
+  },
+  button: {
+    marginTop: 12,
+  },
+  label: {
+    // color: theme.colors.secondary,
+    width: '100%',
+  },
+});
+
+export default memo(ForgotPasswordScreen);
