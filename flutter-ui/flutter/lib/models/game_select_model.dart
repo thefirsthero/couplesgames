ea243cd033cc1107831ebdc7ae5e0498cf5fa253
thefirsthero <vukosi90@gmail.com>
2024-07-14 17:52:57 +0200

    game select model cleanup

--- flutter-ui/flutter/lib/models/game_select_model.dart
+++ flutter-ui/flutter/lib/models/game_select_model.dart
@@ -2,43 +2,35 @@ import 'package:flutter/material.dart';
 
 class GameSelectModel {
   String name;
-  String iconPath;
   String baseRouteName;
-  Color boxColor;
 
   GameSelectModel({
     required this.name,
-    required this.iconPath,
     required this.baseRouteName,
-    required this.boxColor,
   });
 
   static List<GameSelectModel> getGames() {
     List<GameSelectModel> games = [];
 
     games.add(GameSelectModel(
-        name: 'Would You Rather',
-        iconPath: 'assets/icons/plate.svg',
-        baseRouteName: 'would_you_rather',
-        boxColor: Color(0xff9DCEFF)));
+      name: 'Would You Rather',
+      baseRouteName: 'would_you_rather',
+    ));
 
     games.add(GameSelectModel(
-        name: 'Truth or Dare',
-        iconPath: 'assets/icons/pancakes.svg',
-        baseRouteName: 'truth_or_dare',
-        boxColor: Color(0xffEEA4CE)));
+      name: 'Truth or Dare',
+      baseRouteName: 'truth_or_dare',
+    ));
 
     games.add(GameSelectModel(
-        name: 'Never Have I Ever',
-        iconPath: 'assets/icons/pie.svg',
-        baseRouteName: 'never_have_i_ever',
-        boxColor: Color(0xff9DCEFF)));
+      name: 'Never Have I Ever',
+      baseRouteName: 'never_have_i_ever',
+    ));
 
     games.add(GameSelectModel(
-        name: 'Deeper Questions',
-        iconPath: 'assets/icons/orange-snacks.svg',
-        baseRouteName: 'deeper_questions',
-        boxColor: Color(0xffEEA4CE)));
+      name: 'Deeper Questions',
+      baseRouteName: 'deeper_questions',
+    ));
 
     return games;
   }
