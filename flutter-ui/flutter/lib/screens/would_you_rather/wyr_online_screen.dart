cf7351c7a789eab8d628da325621613418678373
thefirsthero <vukosi90@gmail.com>
2024-09-02 21:57:38 +0200

    basic wyr online screen and navigation setup

---
+++ flutter-ui/flutter/lib/screens/would_you_rather/wyr_online_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class WYROnlineScreen extends ConsumerStatefulWidget {
+  const WYROnlineScreen({super.key});
+
+  @override
+  ConsumerState<WYROnlineScreen> createState() => _WYROnlineScreenState();
+}
+
+class _WYROnlineScreenState extends ConsumerState<WYROnlineScreen> {
+  @override
+  void _quitGame() {
+    context.replaceNamed('gameover');
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Would You Rather'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.exit_to_app),
+            onPressed: _quitGame,
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('WYROnlineScreen'),
+      ),
+    );
+  }
+}
