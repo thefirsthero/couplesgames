f807d53314cfea09c1055514baa0f40ffe76cb38
thefirsthero <vukosi90@gmail.com>
2025-06-29 17:12:08 +0200

    Updated Dockerfile and added health check endpoint

---
+++ CouplesGames/CouplesGames.WebAPI/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CouplesGames.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHealthStatus()
+        {
+            return Ok(new { status = "Healthy" });
+        }
+    }
+}
