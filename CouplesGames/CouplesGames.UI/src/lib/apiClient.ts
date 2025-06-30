0cd787c279a3b4f17a6cad3cbf44c1f7697663b1
thefirsthero <vukosi90@gmail.com>
2025-06-30 15:49:35 +0200

    Updated api client to inject auth header on all requests

--- CouplesGames/CouplesGames.UI/src/lib/apiClient.ts
+++ CouplesGames/CouplesGames.UI/src/lib/apiClient.ts
@@ -1,7 +1,20 @@
 import axios from 'axios';
+import { getAuth } from 'firebase/auth';
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+});
+
+apiClient.interceptors.request.use(async (config) => {
+  const auth = getAuth();
+  const currentUser = auth.currentUser;
+
+  if (currentUser) {
+    const token = await currentUser.getIdToken();
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default apiClient;
